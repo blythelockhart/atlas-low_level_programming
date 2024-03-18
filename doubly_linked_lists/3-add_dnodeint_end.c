831ef5a8e1b29723cc298a90c8ad993e11ff0ab2
Blythe Lockhart <blythe.lockhart@att.net>
2024-03-18 22:34:43 +0000

    corrected file 3 - next init

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = *head;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
@@ -26,6 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		dlistint_t *current = *head;
 		while (current->next != NULL)
 			current = current->next;
+
 		current->next = new_node;
 		new_node->prev = current;
 	}
