c37df40529c737724b6eb81b4b84d36f60e5b811
Blythe Lockhart <blythe.lockhart@att.net>
2024-03-18 20:50:10 +0000

    corrected file 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+  * print_dlistint - prints all the elements of a  list.
+  * @h: List head
+  * Return: Number of nodes
+  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
