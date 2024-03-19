ab5d173260b9ca17bc33e4b4909335d18de70952
Blythe Lockhart <blythe.lockhart@att.net>
2024-03-19 02:23:37 +0000

    corrected file 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -13,7 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 		return;
 	printf("{");
 
-	for (i = 0; i < ht->size; i++);
+	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node != NULL)
