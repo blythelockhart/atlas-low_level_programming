b43faf0d14a71744a45e110a5af00193165bc780
Blythe Lockhart <blythe.lockhart@att.net>
2024-03-19 01:36:49 +0000

    corrected file 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,7 +7,9 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
+	unsigned long int i;
+	ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 		return (NULL);
@@ -19,8 +21,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
-	unsigned long int i;
-
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
