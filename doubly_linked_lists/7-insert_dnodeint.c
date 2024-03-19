18ce34b073085ee330147fc033df29aee9048ffa
Blythe Lockhart <blythe.lockhart@att.net>
2024-03-19 00:25:22 +0000

    corrected files 3 and 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,23 +1,41 @@
 #include "lists.h"
 /**
-  * get_dnodeint_at_index - returns the nth node of a list.
-  * @head: List head
-  * @index: N.
-  * Return: Nth element, or NULL.
+  * add_dnodeint_end - inserts a new node at a given position.
+  * @h: List head
+  * @idx: List position
+  * @n: New node
+  * Return: Address of new element, or NULL if it failed.
   */
 
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	dlistint_t *new_node, *tmp;
+	unsigned int i = 0;
 
-	while (head != NULL && count < index)
-	{
-		count++;
-		head = head->next;
-	}
-
-	if (head == NULL || count < index)
+	if (h == NULL)
 		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	tmp = *h;
 
-	return (head);
+	while (tmp != NULL && 1 < idx - 1)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = tmp;
+	new_node->next = tmp->next;
+
+	if (tmp->next != NULL)
+		tmp->next->prev = new_node;
+	tmp->next = new_node;
+
+	return (new_node);
 }
