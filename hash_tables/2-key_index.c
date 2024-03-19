1a8a595d0cbe590ba89cb5a9ede736e75ac76b5b
Blythe Lockhart <blythe.lockhart@att.net>
2024-03-19 01:30:01 +0000

    corrected file 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,12 +1,47 @@
 #include "hash_tables.h"
 /**
- * key_index - Computes the index of a key in the hash table's array.
- * @key: The key string.
- * @size: Size of the array of the hash table.
- * Return: The index at which the key/value pair should be stored in the array.
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table
+ * @key: The key to add
+ * @value: The value associated
+ * Return: 1 if it succeeded, 0 otherwise.
  */
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    return (hash_djb2(key) % size);
+	unsigned long int indeax;
+	hash_node_t *new_node, *tmp;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			if (tmp->value == NULL)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
